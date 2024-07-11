52ee1c5d928529cc224a49b5c0f58e97fa218724
asanchezba <asanchezba@gmail.com>
2024-07-11 20:29:15 +0200

    updated

--- 01_Day_Introduction/Helloworld.py
+++ 01_Day_Introduction/Helloworld.py
@@ -1,1 +1,17 @@
+#Day 1 - 30 Days of Python Challenge
+
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 / 4)
+print(3 % 4) #modulus
+print(3 ** 4) #exponential
+print( 3 // 4) #floor divisor operator
+
+print('Anna')
+print('Sánchez')
+print('Spain')
+print('I am enjoying 30 days of python')
+
+print(type(10))
 
