3b454762092f0ab60b20b06c903a063e495a4f32
asanchezba <asanchezba@gmail.com>
2024-07-12 20:22:46 +0200

    updated

---
+++ 02_Day_Variable/Variables.py
@@ -0,0 +1,10 @@
+#Day 2 - 30 Days of Python Challenge
+
+print(len('Hello world')) #counts the number of characters including space
+print(type('Hello world')) #checks data type
+number_to_string = str(10) #converts number to string
+print(type(number_to_string))
+string_to_number = int('10') #converts string to number
+print(type(string_to_number))
+
+
