8892e0396dc567192f3fd84009a4da117a5b97a3
asanchezba <asanchezba@gmail.com>
2024-07-13 08:23:59 +0200

    updated

--- 02_Day_Variable/Variables.py
+++ 02_Day_Variable/Variables.py
@@ -1,10 +1,18 @@
 #Day 2 - 30 Days of Python Challenge
 
+#Build in functions
 print(len('Hello world')) #counts the number of characters including space
 print(type('Hello world')) #checks data type
 number_to_string = str(10) #converts number to string
 print(type(number_to_string))
 string_to_number = int('10') #converts string to number
 print(type(string_to_number))
+#input('Enter you name: ') takes users input
+
+#help('keywords') #prints all python reserved words
+#help('str') #give info about strings
+
+print(min(20,30,40,50))
+
 
 
