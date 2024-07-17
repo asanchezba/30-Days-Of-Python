a40e9d1b41a150d0223768cd82e725c55d7a764a
asanchezba <asanchezba@gmail.com>
2024-07-17 20:09:12 +0200

    updated

---
+++ 05_Day_Lists/Lists.py
@@ -0,0 +1,10 @@
+### Day 5 - 30 Days of Python Challenge ###
+
+# Lists #
+
+my_list = list()
+my_other_list = []
+
+fruits = ['banana', 'mango', 'apple', 'kiwi']
+print('Fruits:', fruits)
+print('Number of fruits:', len(fruits))
