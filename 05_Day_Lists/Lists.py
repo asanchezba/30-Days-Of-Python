0593545143b216168cf96de1feb833dd3b3fc5ee
asanchezba <asanchezba@gmail.com>
2024-07-19 20:18:13 +0200

    updated

--- 05_Day_Lists/Lists.py
+++ 05_Day_Lists/Lists.py
@@ -8,3 +8,17 @@ my_other_list = []
 fruits = ['banana', 'mango', 'apple', 'kiwi']
 print('Fruits:', fruits)
 print('Number of fruits:', len(fruits))
+
+# Accessing list items
+first_fruit = fruits[0]
+print(first_fruit)
+last_fruit = fruits[-1]
+print(last_fruit)
+
+# Unpacking list items
+first_fruit, second_fruit, *rest = fruits
+print(first_fruit)
+print(second_fruit)
+print(rest)
+
+# Slicing items from a List
