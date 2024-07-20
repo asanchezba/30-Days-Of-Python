f076fa288b2b7eefc3143ec5cbcea329af461fcf
asanchezba <asanchezba@gmail.com>
2024-07-20 19:39:22 +0200

    updated

--- 05_Day_Lists/Lists.py
+++ 05_Day_Lists/Lists.py
@@ -22,3 +22,12 @@ print(second_fruit)
 print(rest)
 
 # Slicing items from a List
+all_fruits = fruits[0:4] # it returns all the fruits
+all_fruits = fruits[0:] # if we don't set where to stop it takes all the rest
+print(all_fruits)
+mango_and_apple = fruits[1:3] # it does not include the first index
+print(mango_and_apple)
+mango_apple_kiwi = fruits[1:]
+print(mango_apple_kiwi)
+banana_and_apple = fruits[::2] # here we used a 3rd argument, step. It will take every 2cnd item
+print(banana_and_apple)
