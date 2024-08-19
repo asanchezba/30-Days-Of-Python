b486d716a111213f18869f8dedd3f8b8ae2bb9f1
asanchezba <asanchezba@gmail.com>
2024-08-19 16:27:39 +0200

    updated

---
+++ 06_Day_Tuples/Tuples.py
@@ -0,0 +1,27 @@
+### Day 6 - 30 Days of Python Challenge ###
+
+# Creating a Tuple
+empty_tuple = ()
+tpl = ('item1', 'item2', 'item3')
+print(len(tpl))
+
+# Accessing Tuple Items
+fruits = ('banana', 'orange', 'mango', 'lemon')
+first_fruit = fruits[0]
+print(first_fruit)
+second_fruit = fruits[1]
+print(second_fruit)
+last_index =len(fruits) - 1
+print(last_index)
+last_fruit = fruits[last_index]
+print(last_fruit)
+
+fruits = ('banana', 'orange', 'mango', 'lemon')
+first_fruit = fruits[-4]
+print(first_fruit)
+second_fruit = fruits[-3]
+print(second_fruit)
+last_fruit = fruits[-1]
+print(last_fruit)
+
+# Slicing tuples
