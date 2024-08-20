f73e6eac766577a4b9487379ad3ecdddb629069a
asanchezba <asanchezba@gmail.com>
2024-08-20 17:07:33 +0200

    updated

--- 06_Day_Tuples/Tuples.py
+++ 06_Day_Tuples/Tuples.py
@@ -34,3 +34,32 @@ orange_mango = fruits[1:3]  # doesn't include itesm from index 3 to last
 print(orange_mango)
 orange_to_the_rest = fruits[1:]
 print(orange_to_the_rest)
+
+fruits = ('banana', 'orange', 'mango', 'lemon')
+all_fruits = fruits[-4:]    # all items
+orange_mango = fruits[-3:-1]  # doesn't include itesm from index 3 to last
+orange_to_the_rest = fruits[-3:]
+
+# Changing Tuples to Lists
+fruits = ('banana', 'orange', 'mango', 'lemon')
+fruits = list(fruits)
+fruits[0] = 'apple' # Tuple is immutable if we want to modify a tuple we should change it to a list
+print(fruits)    
+fruits = tuple(fruits)
+print(fruits)    
+
+# Checking an Item in a Tuple
+fruits = ('banana', 'orange', 'mango', 'lemon')
+print('orange' in fruits) # True
+print('apple' in fruits) # False
+
+# Joining Tuples
+fruits = ('banana', 'orange', 'mango', 'lemon')
+vegetables = ('Tomato', 'Potato', 'Cabbage','Onion', 'Carrot')
+fruits_and_vegetables = fruits + vegetables
+print(fruits_and_vegetables)
+
+# Deleting Tuples
+fruits = ('banana', 'orange', 'mango', 'lemon')
+del fruits # It is not possible to remove a single item in a tuple but it is possible to delete the tuple itself 
+
