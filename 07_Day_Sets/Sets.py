7cd8d1ded4ad765db0dff4f36b040a1ac2ae7d13
asanchezba <asanchezba@gmail.com>
2024-08-20 17:58:03 +0200

    updated

--- 07_Day_Sets/Sets.py
+++ 07_Day_Sets/Sets.py
@@ -4,3 +4,20 @@
 empty_set = set()
 fruits = {'banana', 'orange', 'mango', 'lemon'}
 
+# Getting Set's Length
+print(len(fruits))
+
+# Checking an Item
+fruits = {'banana', 'orange', 'mango', 'lemon'}
+print('mango' in fruits )
+
+# Adding Items to a Set
+fruits = {'banana', 'orange', 'mango', 'lemon'}
+fruits.add('lime')
+print(fruits)
+
+fruits = {'banana', 'orange', 'mango', 'lemon'}
+vegetables = ('tomato', 'potato', 'cabbage','onion', 'carrot')
+fruits.update(vegetables) # to add multiple items to a set
+print(fruits)
+
