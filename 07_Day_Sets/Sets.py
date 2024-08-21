bc509b5c08f1fce1eef011e376185cf1268b7d1f
asanchezba <asanchezba@gmail.com>
2024-08-21 15:53:20 +0200

    updated

--- 07_Day_Sets/Sets.py
+++ 07_Day_Sets/Sets.py
@@ -113,5 +113,42 @@ A = {19, 22, 24, 20, 25, 26}
 B = {19, 22, 20, 25, 26, 24, 28, 27}
 age = [22, 19, 24, 25, 26, 24, 25, 24]
 
+# 1.Find the length of the set it_companies
+print(len(it_companies))
 
+# 2.Add 'Twitter' to it_companies
+it_companies.add('Twitter')
+print(it_companies)
+
+# 3.Insert multiple IT companies at once to the set it_companies
+additional_companies = {'SAP', 'Juniper', 'Git', 'bmc', 'ebay', 'PayPal'}
+it_companies.update(additional_companies)
+print(it_companies)
+
+# 4.Remove one of the companies from the set it_companies
+it_companies.remove('bmc')
+print(it_companies)
+
+# 5.Join A and B
+print(A.union(B))
+
+# 6.Find A intersection B
+print(A.intersection(B))
+
+# 7.Is A subset of B
+print(A.issubset(B))
+
+# 8.Are A and B disjoint sets
+print(A.isdisjoint(B))
+
+# 9.What is the symmetric difference between A and B
+print(A.symmetric_difference(B))
+
+# 10.Convert the ages to a set and compare the length of the list and the set, which one is bigger?
+age_set = set(age)
+print(len(age_set))
+print(len(age))
+
+# 11.Delete the sets completely
+del A, B, it_companies
 
