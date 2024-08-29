dab76f01b3a3344ff5e0d215c14486dabe804da7
asanchezba <asanchezba@gmail.com>
2024-08-29 16:34:19 +0200

    updated

---
+++ 12_Day_Modules/Modules.py
@@ -0,0 +1,11 @@
+### Day 12 - 30 Days of Python Challenge ###
+
+# Creating a Module
+''' To create a module we write our codes in a python script and 
+we save it as a .py file.'''
+
+# Importing a Module
+import mymodule
+print(mymodule.generate_full_name('Anna', 'Sánchez'))
+
+# Import Functions from a Module
