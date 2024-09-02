3f8e930609f4aeafd72294fd6447e74729888197
asanchezba <asanchezba@gmail.com>
2024-09-02 20:08:51 +0200

    updated

--- 13_Day_List_Comprehension/List_Comprehension.py
+++ 13_Day_List_Comprehension/List_Comprehension.py
@@ -49,3 +49,13 @@ numbers = [-8, -7, -3, -1, 0, 1, 3, 4, 5, 7, 6, 8, 10]
 positive_even_numbers = [i for i in range(21) if i % 2 == 0 and i > 0]
 print(positive_even_numbers)
 
+# Flattening a three dimensional array
+list_of_lists = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+flattened_list = [ number for row in list_of_lists for number in row]
+print(flattened_list) 
+
+# Lambda Function
+'''A small anonymous function without a name. 
+It can take any number of arguments, but can only have one expression.'''
+
+
