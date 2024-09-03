6d58905bfebc8f9b0d3fb1dabaa42020c9e9fc3c
asanchezba <asanchezba@gmail.com>
2024-09-03 12:05:16 +0200

    updated

--- 13_Day_List_Comprehension/Exercicies.py
+++ 13_Day_List_Comprehension/Exercicies.py
@@ -1,3 +1,26 @@
 # Exercicies
 
 # 1.Filter only negative and zero in the list using list comprehension
+numbers = [-4, -3, -2, -1, 0, 2, 4, 6]
+negative_zero_numbers = [i for i in numbers if i <= 0 ]
+print(negative_zero_numbers)
+
+# 2.Flatten the following list of lists of lists to a one dimensional list
+list_of_lists =[[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+flattened_list = [ number for row in list_of_lists for number in row]
+print(flattened_list) 
+
+# 3.Using list comprehension create the following list of tuples
+#[(0, 1, 0, 0, 0, 0, 0),
+#(1, 1, 1, 1, 1, 1, 1),
+#(2, 1, 2, 4, 8, 16, 32),
+#(3, 1, 3, 9, 27, 81, 243),
+#(4, 1, 4, 16, 64, 256, 1024),
+#(5, 1, 5, 25, 125, 625, 3125),
+#(6, 1, 6, 36, 216, 1296, 7776),
+#(7, 1, 7, 49, 343, 2401, 16807),
+#(8, 1, 8, 64, 512, 4096, 32768),
+#(9, 1, 9, 81, 729, 6561, 59049),
+#(10, 1, 10, 100, 1000, 10000, 100000)]
+
+
