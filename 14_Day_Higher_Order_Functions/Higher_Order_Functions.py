83b3e0cba6e7e3deab06d0d50682104be5dd363d
asanchezba <asanchezba@gmail.com>
2024-09-05 13:40:19 +0200

    updated

--- 14_Day_Higher_Order_Functions/Higher_Order_Functions.py
+++ 14_Day_Higher_Order_Functions/Higher_Order_Functions.py
@@ -21,3 +21,45 @@ result = higher_order_function(sum_numbers, [1, 2, 3, 4, 5])
 print(result) 
 
 # Function as a Return Value
+def square(x):         
+    return x ** 2
+
+def cube(x):            
+    return x ** 3
+
+def absolute(x):        
+    if x >= 0:
+        return x
+    else:
+        return -(x)
+
+def higher_order_function(type): # a higher order function returning a function
+    if type == 'square':
+        return square
+    elif type == 'cube':
+        return cube
+    elif type == 'absolute':
+        return absolute
+
+result = higher_order_function('square')
+print(result(3))       
+result = higher_order_function('cube')
+print(result(3))       
+result = higher_order_function('absolute')
+print(result(-3))
+
+# Python Closures
+'''Closure is created by nesting a function inside another 
+encapsulating function and then returning the inner function.'''
+
+def add_ten():
+    ten = 10
+    def add(num):
+        return num + ten
+    return add
+
+closure_result = add_ten()
+print(closure_result(5))  
+print(closure_result(10))  
+
+# Python Decorators
