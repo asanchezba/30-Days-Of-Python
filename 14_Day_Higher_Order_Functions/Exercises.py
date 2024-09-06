e248693649a9b1f239503f4dc58d8bc16900e36d
asanchezba <asanchezba@gmail.com>
2024-09-06 20:37:31 +0200

    updated

---
+++ 14_Day_Higher_Order_Functions/Exercises.py
@@ -0,0 +1,7 @@
+# Exercises
+
+countries = ['Estonia', 'Finland', 'Sweden', 'Denmark', 'Norway', 'Iceland']
+names = ['Josh', 'Max', 'Matt', 'Dan']
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# 1.Use map to create a new list by changing each country to uppercase in the countries list
