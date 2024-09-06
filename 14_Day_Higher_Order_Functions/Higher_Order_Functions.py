5ca160c8e852396fe0cc9897b8f2ef8a04fbca05
asanchezba <asanchezba@gmail.com>
2024-09-06 20:32:35 +0200

    updated

--- 14_Day_Higher_Order_Functions/Higher_Order_Functions.py
+++ 14_Day_Higher_Order_Functions/Higher_Order_Functions.py
@@ -133,3 +133,80 @@ print_full_name("Anna", "Sánchez",'Spain')
 
 # Built-in Higher Order Functions
 # Map Function
+'''syntax
+map(function, iterable)'''
+
+numbers = [1, 2, 3, 4, 5] # iterable
+def square(x):
+    return x ** 2
+numbers_squared = map(square, numbers)
+print(list(numbers_squared))   
+# Lets apply it with a lambda function
+numbers_squared = map(lambda x : x ** 2, numbers)
+print(list(numbers_squared)) 
+
+numbers_str = ['1', '2', '3', '4', '5']  # iterable
+numbers_int = map(int, numbers_str)
+print(list(numbers_int)) 
+
+names = ['Josh', 'Max', 'Matt', 'Dan']  # iterable
+
+def change_to_upper(name):
+    return name.upper()
+
+names_upper_cased = map(change_to_upper, names)
+print(list(names_upper_cased))    
+
+# Let us apply it with a lambda function
+names_upper_cased = map(lambda name: name.upper(), names)
+print(list(names_upper_cased)) 
+
+# Filter Function
+'''The filter() function calls the specified function 
+which returns boolean for each item of the specified 
+iterable (list). It filters the items that satisfy the 
+filtering criteria.'''
+
+'''syntax
+filter(function, iterable)'''
+
+numbers = [1, 2, 3, 4, 5]  # iterable
+
+def is_even(num):
+    if num % 2 == 0:
+        return True
+    return False
+
+even_numbers = filter(is_even, numbers)
+print(list(even_numbers))       
+
+
+def is_odd(num):
+    if num % 2 != 0:
+        return True
+    return False
+
+odd_numbers = filter(is_odd, numbers)
+print(list(odd_numbers))
+
+names = ['Josh', 'Max', 'Matt', 'Dan']
+def is_name_long(name):
+    if len(name) > 3:
+        return True
+    return False
+
+long_names = filter(is_name_long, names)
+print(list(long_names))         
+
+# Reduce Function
+'''Like map and filter it takes two parameters, a 
+function and an iterable. However, it does not 
+return another iterable, instead it returns a single value'''
+
+from functools import reduce
+numbers_str = ['1', '2', '3', '4', '5']  # iterable
+def add_two_nums(x, y):
+    return int(x) + int(y)
+
+total = reduce(add_two_nums, numbers_str)
+print(total)
