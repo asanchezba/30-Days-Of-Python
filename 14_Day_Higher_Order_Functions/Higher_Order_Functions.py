700364caf174b2df56f1a6e3b27bfe1bd7991f0e
asanchezba <asanchezba@gmail.com>
2024-09-06 13:41:30 +0200

    updated

--- 14_Day_Higher_Order_Functions/Higher_Order_Functions.py
+++ 14_Day_Higher_Order_Functions/Higher_Order_Functions.py
@@ -119,3 +119,17 @@ def greeting():
 print(greeting()) 
 
 # Accepting Parameters in Decorator Functions
+def decorator_with_parameters(function):
+    def wrapper_accepting_parameters(para1, para2, para3):
+        function(para1, para2, para3)
+        print("I live in {}".format(para3))
+    return wrapper_accepting_parameters
+
+@decorator_with_parameters
+def print_full_name(first_name, last_name, country):
+    print("I am {} {}. I love to read.".format(first_name, last_name, country))
+
+print_full_name("Anna", "Sánchez",'Spain')
+
+# Built-in Higher Order Functions
+# Map Function
