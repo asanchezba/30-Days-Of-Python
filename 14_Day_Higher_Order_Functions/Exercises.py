7d27db99e6a9460bfb8df6da652349f0930f9d77
asanchezba <asanchezba@gmail.com>
2024-09-06 20:41:22 +0200

    updated

--- 14_Day_Higher_Order_Functions/Exercises.py
+++ 14_Day_Higher_Order_Functions/Exercises.py
@@ -5,3 +5,10 @@ names = ['Josh', 'Max', 'Matt', 'Dan']
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 # 1.Use map to create a new list by changing each country to uppercase in the countries list
+def change_to_upper(name):
+    return name.upper()
+
+countries_upper_case = map(change_to_upper, countries)
+print(list(countries_upper_case))   
+
+# 2.Use map to create a new list by changing each number to its square in the numbers list
