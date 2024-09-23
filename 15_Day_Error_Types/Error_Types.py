907e78149cefe8c306651f40375c724658fb9184
asanchezba <asanchezba@gmail.com>
2024-09-23 20:50:25 +0200

    updated

---
+++ 15_Day_Error_Types/Error_Types.py
@@ -0,0 +1,16 @@
+### Day 15 - 30 Days of Python Challenge ###
+
+# SyntaxError
+#print 'hello world'
+print('hello world')
+
+# NameError
+#print(age) # NameError: name 'age' is not defined
+age = 28
+print(age)
+
+# IndexError
+numbers = [1, 2, 3, 4, 5]
+#numbers[5] #IndexError: list index out of range
+
+# ModuleNotFoundError
