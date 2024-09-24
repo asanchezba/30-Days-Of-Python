4ee0aeb01b392d555e009e409ac6de0a51824bf9
asanchezba <asanchezba@gmail.com>
2024-09-24 11:08:29 +0200

    updated

--- 15_Day_Error_Types/Error_Types.py
+++ 15_Day_Error_Types/Error_Types.py
@@ -14,3 +14,38 @@ numbers = [1, 2, 3, 4, 5]
 #numbers[5] #IndexError: list index out of range
 
 # ModuleNotFoundError
+#import maths #ModuleNotFoundError: No module named 'maths'
+import math 
+
+# AttributeError
+import math 
+#math.PI #AttributeError: module 'math' has no attribute 'PI'
+math.pi
+
+# KeyError
+users = {'name':'Anna', 'age':28, 'country':'UK'}
+users['name']
+#users['county'] #KeyError: 'county'
+users = ['country']
+
+# TypeError
+#4 + '3' #TypeError: unsupported operand type(s) for +: 'int' and 'str'
+4 + int('3')
+4 + float('3')
+
+# ImportError
+#from math import power # ImportError: cannot import name 'power' from 'math' 
+from math import pow
+pow(2,3)
+
+# ValueError
+#int('12a') # ValueError: invalid literal for int() with base 10: '12a'
+
+# ZeroDivisionError
+#1/0 # ZeroDivisionError: division by zero
+
+
+
+
+
+
