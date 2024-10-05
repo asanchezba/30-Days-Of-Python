ac584635ea40b8f728ce062ab03d50d4af086fde
asanchezba <asanchezba@gmail.com>
2024-10-05 18:32:21 +0200

    updated

--- 16_Day_Date_Time/Date_Time.py
+++ 16_Day_Date_Time/Date_Time.py
@@ -23,3 +23,35 @@ print('timestamp', timestamp)
 print(f'{day}/{month}/{year}, {hour}:{minute}')
 
 # Formatting Date Output Using strftime
+from datetime import datetime
+new_year = datetime(2025, 1, 1)
+print(new_year)
+day = new_year.day
+month = new_year.month
+year = new_year.year
+hour = new_year.hour
+minute = new_year.minute
+second = new_year.second
+print(day, month, year, hour, minute)
+print(f'{day}/{month}/{year}, {hour}:{minute}')
+
+from datetime import datetime
+# current date and time
+now = datetime.now()
+t = now.strftime("%H:%M:%S")
+print("time:", t)
+time_one = now.strftime("%m/%d/%Y, %H:%M:%S")
+# mm/dd/YY H:M:S format
+print("time one:", time_one)
+time_two = now.strftime("%d/%m/%Y, %H:%M:%S")
+# dd/mm/YY H:M:S format
+print("time two:", time_two)
+
+# String to Time Using strptime
+from datetime import datetime
+date_string = "2 October, 2024"
+print("date_string =", date_string)
+date_object = datetime.strptime(date_string, "%d %B, %Y")
+print("date_object =", date_object)
+
+# Using date from datetime
