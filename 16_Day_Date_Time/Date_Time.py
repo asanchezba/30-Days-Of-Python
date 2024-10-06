91a6d17d161cada2a18c61ac9d7260f1feb858d5
asanchezba <asanchezba@gmail.com>
2024-10-06 13:17:43 +0200

    updated

--- 16_Day_Date_Time/Date_Time.py
+++ 16_Day_Date_Time/Date_Time.py
@@ -55,3 +55,45 @@ date_object = datetime.strptime(date_string, "%d %B, %Y")
 print("date_object =", date_object)
 
 # Using date from datetime
+from datetime import date
+d = date(2024, 1, 1)
+print(d)
+print('Current date:', d.today())
+# date object of today's date
+today = date.today()
+print("Current year:", today.year)   
+print("Current month:", today.month) 
+print("Current day:", today.day) 
+
+# Time Objects to Represent Time
+from datetime import time
+# time(hour = 0, minute = 0, second = 0)
+a = time()
+print('a =', a)
+# time(hour, minute and second)
+b = time(10, 30, 50)
+print("b =", b)
+# time(hour, minute and second)
+c = time(hour=10, minute=30, second=50)
+print("c =", c)
+# time(hour, minute, second, microsecond)
+d = time(10, 30, 50, 200555)
+print("d =", d)
+
+# Difference Between Two Points in Time Using
+today = date(year=2024, month=10, day=6)
+new_year = date(year=2025, month=1, day=1)
+time_left_for_newyear = new_year - today
+print('Time left for new year: ', time_left_for_newyear)
+
+t1 = datetime(year = 2024, month = 10, day = 6, hour = 0, minute = 59, second = 0)
+t2 = datetime(year = 2025, month = 1, day = 1, hour = 0, minute = 0, second = 0)
+diff = t2 - t1
+print('Time left for new year:', diff)
+
+# Difference Between Two Points in Time Using timedelata
+from datetime import timedelta
+t1 = timedelta(weeks=12, days=10, hours=4, seconds=20)
+t2 = timedelta(days=7, hours=5, minutes=3, seconds=30)
+t3 = t1 - t2
+print("t3 =", t3)
