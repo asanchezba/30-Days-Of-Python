452973bee5bcff2239287efc3c095693ad51d676
asanchezba <asanchezba@gmail.com>
2024-10-25 20:23:48 +0200

    updated

--- 17_Day_Exception_Handling/Exception_Handling.py
+++ 17_Day_Exception_Handling/Exception_Handling.py
@@ -19,3 +19,28 @@ except ZeroDivisionError:
     print('zero division error occured')
 
 # Packing and Unpacking Arguments in Python
+'''We use two operators:
+* for tuples
+** for diccionaries'''
+
+# Unpacking lists
+#def sum_of_five_nums(a, b, c, d, e):
+    #return a + b + c + d + e
+
+#lst = [1, 2, 3, 4, 5]
+#print(sum_of_five_nums(lst)) # TypeError: sum_of_five_nums() missing 4 required positional arguments: 'b', 'c', 'd', and 'e'
+
+def sum_of_five_nums(a, b, c, d, e):
+    return a + b + c + d + e
+
+lst = [1, 2, 3, 4, 5]
+print(sum_of_five_nums(*lst)) #15
+
+numbers = range(2, 7)  # normal call with separate arguments
+print(list(numbers)) # [2, 3, 4, 5, 6]
+args = [2, 7]
+numbers = range(*args)  # call with arguments unpacked from a list
+print(numbers) 
+
+
+
